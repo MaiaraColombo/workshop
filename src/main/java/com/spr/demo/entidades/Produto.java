01d37a3b3f56be70ba67c78f994e4c086b3c5ffa
Maiara Colombo <maiara.ccolombo@outlook.com>
2023-06-12 17:23:30 -0300

    Produto - PedidoItem

--- src/main/java/com/spr/demo/entidades/Produto.java
+++ src/main/java/com/spr/demo/entidades/Produto.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +34,9 @@ public class Produto implements Serializable {
 	@JoinTable(name = "tb_produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private Set<Categoria> categorias = new HashSet<>();
 
+	@OneToMany(mappedBy = "id.produto")
+	private Set<PedidoItem> itens = new HashSet<>();
+	
 	public Produto() {
 
 	}
@@ -88,6 +94,16 @@ public class Produto implements Serializable {
 		return categorias;
 	}
 
+	@JsonIgnore
+	public Set<Produto> getProdutos(){
+		Set<Produto> set = new HashSet<>();
+		for(PedidoItem x : itens) {
+			set.add(x.getProduto());
+		}
+		return set;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
