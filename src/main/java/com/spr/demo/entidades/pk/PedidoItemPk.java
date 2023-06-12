3206accd2400a9b8557d8f09a5749985b6184acc
Maiara Colombo <maiara.ccolombo@outlook.com>
2023-06-12 14:18:40 -0300

    Pedido Item

---
+++ src/main/java/com/spr/demo/entidades/pk/PedidoItemPk.java
@@ -0,0 +1,58 @@
+package com.spr.demo.entidades.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.spr.demo.entidades.Pedido;
+import com.spr.demo.entidades.Produto;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+@Embeddable
+public class PedidoItemPk implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "pedido_id")
+	private Pedido pedido;
+	private Produto produto;
+	
+	@ManyToOne
+	@JoinColumn(name = "produto_id")
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(pedido, produto);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PedidoItemPk other = (PedidoItemPk) obj;
+		return Objects.equals(pedido, other.pedido) && Objects.equals(produto, other.produto);
+	}
+
+}
