687689a577e9c9bcaf390a003a9411b6dd662758
Maiara Colombo <maiara.ccolombo@outlook.com>
2023-06-12 19:26:48 -0300

    Total e SubTotal

--- src/main/java/com/spr/demo/entidades/Pedido.java
+++ src/main/java/com/spr/demo/entidades/Pedido.java
@@ -102,6 +102,14 @@ public class Pedido implements Serializable {
 	public Set<PedidoItem> getItens() {
 		return itens;
 	}
+	
+	public Double getTotal(){
+		double soma = 0.0;
+		for (PedidoItem x : itens) {
+			soma += x.getSubTotal();
+		}
+		return soma;
+	}
 
 	@Override
 	public int hashCode() {
--- src/main/java/com/spr/demo/entidades/PedidoItem.java
+++ src/main/java/com/spr/demo/entidades/PedidoItem.java
@@ -69,6 +69,11 @@ public class PedidoItem implements Serializable {
 		this.preco = preco;
 	}
 
+	
+	public Double getSubTotal(){
+		return preco * quantidade;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
